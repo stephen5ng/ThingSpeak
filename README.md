29e53fd4c53be24b12fd96ae722d0e7c10a2b5a6
stephen5ng <110236404+stephen5ng@users.noreply.github.com>
2022-09-08 15:01:45 -0700

    Convert to support 7080.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ThingSpeak
+
+platformio.ini should look something like this:
+
+[env:mayfly]
+monitor_speed = 115200
+platform = atmelavr
+board = mayfly
+framework = arduino
+lib_ignore = RTCZero
+lib_ldf_mode = deep+
+build_flags =
+    -DSDI12_EXTERNAL_PCINT
+lib_deps =
+    EnviroDIY_ModularSensors
+    mathworks/ThingSpeak@^2.0.0
--- ThingSpeak.ino
+++ ThingSpeak.ino
@@ -1,30 +1,58 @@
-/*
-  ReadField
-
-  Description: Demonstates reading from a public channel which doesn't not require an API key and reading from a private channel which does requires a read API key.
-               The value read from the public channel is the current outside temperature at MathWorks headquaters in Natick, MA.  The value from the
-               private channel is an example counter that increments every 10 seconds.
-
-  Hardware:  Arduino Ethernet
-
-  !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
-
-  Note:
-  - Requires the Ethernet library
-
-  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and
-  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.
-
-  Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
-  See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
-
-  For licensing information, see the accompanying license file.
-
-  Copyright 2020, The MathWorks, Inc.
-*/
-
 #include "secrets.h"
 #include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+
+// ==========================================================================
+//  Data Logging Options
+// ==========================================================================
+/** Start [logging_options] */
+
+// Set the input and output pins for the logger
+// NOTE:  Use -1 for pins that do not apply
+const int32_t serialBaud = 115200; // Baud rate for debugging
+const int8_t greenLED = 8;         // Pin for the green LED
+const int8_t redLED = 9;           // Pin for the red LED
+const int8_t buttonPin = 21;       // Pin for debugging mode (ie, button pin)
+const int8_t wakePin = 31;         // MCU interrupt/alarm pin to wake from sleep
+// Mayfly 0.x D31 = A7
+// Set the wake pin to -1 if you do not want the main processor to sleep.
+// In a SAMD system where you are using the built-in rtc, set wakePin to 1
+const int8_t sdCardPwrPin = -1;   // MCU SD card power pin
+const int8_t sdCardSSPin = 12;    // SD card chip select/slave select pin
+const int8_t sensorPowerPin = 22; // MCU pin controlling main sensor power
+/** End [logging_options] */
+
+// ==========================================================================
+//  Wifi/Cellular Modem Options
+// ==========================================================================
+/** Start [sim7080] */
+// For almost anything based on the SIMCom SIM7080G
+#include <modems/SIMComSIM7080.h>
+
+#define modemSerial Serial1
+
+// NOTE: Extra hardware and software serial ports are created in the "Settings
+// for Additional Serial Ports" section
+const int32_t modemBaud = 9600; //  SIM7080 does auto-bauding by default, but for simplicity we set to 9600
+
+// Modem Pins - Describe the physical pin connection of your modem to your board
+// NOTE:  Use -1 for pins that do not apply
+
+const int8_t modemVccPin = 18;     // MCU pin controlling modem power --- Pin 18 is the power enable pin for the bee socket on Mayfly v1.0,
+                                   //  use -1 if using Mayfly 0.5b or if the bee socket is constantly powered (ie you changed SJ18 on Mayfly1.0 to 3.3v)
+const int8_t modemStatusPin = 19;  // MCU pin used to read modem status
+const int8_t modemSleepRqPin = 23; // MCU pin for modem sleep/wake request
+const int8_t modemLEDPin = redLED; // MCU pin connected an LED to show modem
+                                   // status
+
+// Network connection information
+const char *apn = "hologram"; // APN connection name, typically Hologram unless you have a different provider's SIM card. Change as needed
+
+// Create the modem object
+SIMComSIM7080 modem7080(&modemSerial, modemVccPin, modemStatusPin,
+                        modemSleepRqPin, apn);
+// Create an extra reference to the modem by a generic name
+SIMComSIM7080 modem = modem7080;
+/** End [sim7080] */
 
 // Weather station channel details
 unsigned long weatherStationChannelNumber = SECRET_CH_ID_WEATHER_STATION;
@@ -38,19 +66,42 @@ unsigned int counterFieldNumber = 1;
 void setup()
 {
     Serial.begin(115200); // Initialize serial
+    Serial.println("setup");
 
-    //   ThingSpeak.begin(client);  // Initialize ThingSpeak
+    modemSerial.begin(modemBaud);
+    modem.setModemLED(modemLEDPin);
+    modem.setModemWakeLevel(HIGH);  // ModuleFun Bee inverts the signal
+    modem.setModemResetLevel(HIGH); // ModuleFun Bee inverts the signal
+    Serial.println(F("Waking modem and setting Cellular Carrier Options..."));
+    modem.modemWake();                  // NOTE:  This will also set up the modem
+    modem.gsmModem.setBaud(modemBaud);  // Make sure we're *NOT* auto-bauding!
+    modem.gsmModem.setNetworkMode(38);  // set to LTE only
+    modem.gsmModem.setPreferredMode(1); // set to CAT-M
+
+    ThingSpeak.begin(modem.gsmClient); // Initialize ThingSpeak
+    Serial.println("setup done");
 }
 
 void loop()
 {
+    Serial.println("looping");
 
-    int statusCode = 0;
+    if (!modem.modemWake())
+    {
+        Serial.println(F("Could not wake modem."));
+        return;
+    }
+    if (!modem.connectInternet())
+    {
+        Serial.println(F("Could not connect to internet."));
+        return;
+    }
 
     // Read in field 4 of the public channel recording the temperature
     float temperatureInF = ThingSpeak.readFloatField(weatherStationChannelNumber, temperatureFieldNumber);
 
     // Check the status of the read operation to see if it was successful
+    int statusCode = 0;
     statusCode = ThingSpeak.getLastReadStatus();
     if (statusCode == 200)
     {
