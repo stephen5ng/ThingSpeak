1793be41bfed6880e7d0551a063c7529cd3d67aa
stephen5ng <110236404+stephen5ng@users.noreply.github.com>
2022-09-08 14:20:31 -0700

    Reformat per Visual Code.

--- ThingSpeak.ino
+++ ThingSpeak.ino
@@ -1,25 +1,25 @@
 /*
   ReadField
-  
+
   Description: Demonstates reading from a public channel which doesn't not require an API key and reading from a private channel which does requires a read API key.
                The value read from the public channel is the current outside temperature at MathWorks headquaters in Natick, MA.  The value from the
                private channel is an example counter that increments every 10 seconds.
-  
+
   Hardware:  Arduino Ethernet
-  
+
   !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
-  
-  Note:  
+
+  Note:
   - Requires the Ethernet library
-  
-  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and 
-  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.  
-  
+
+  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and
+  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.
+
   Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
   See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
-  
+
   For licensing information, see the accompanying license file.
-  
+
   Copyright 2020, The MathWorks, Inc.
 */
 
@@ -32,45 +32,50 @@ unsigned int temperatureFieldNumber = 4;
 
 // Counting channel details
 unsigned long counterChannelNumber = SECRET_CH_ID_COUNTER;
-const char * myCounterReadAPIKey = SECRET_READ_APIKEY_COUNTER;
-unsigned int counterFieldNumber = 1; 
+const char *myCounterReadAPIKey = SECRET_READ_APIKEY_COUNTER;
+unsigned int counterFieldNumber = 1;
 
-void setup() {
-  Serial.begin(115200);  //Initialize serial
- 
-//   ThingSpeak.begin(client);  // Initialize ThingSpeak
+void setup()
+{
+    Serial.begin(115200); // Initialize serial
+
+    //   ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
 
-void loop() {
+void loop()
+{
 
-  int statusCode = 0;
-  
-  // Read in field 4 of the public channel recording the temperature
-  float temperatureInF = ThingSpeak.readFloatField(weatherStationChannelNumber, temperatureFieldNumber);  
+    int statusCode = 0;
 
-  // Check the status of the read operation to see if it was successful
-  statusCode = ThingSpeak.getLastReadStatus();
-  if(statusCode == 200){
-    Serial.println("Temperature at MathWorks HQ: " + String(temperatureInF) + " deg F");
-  }
-  else{
-    Serial.println("Problem reading channel. HTTP error code " + String(statusCode)); 
-  }
-  
-  delay(15000); // No need to read the temperature too often.
+    // Read in field 4 of the public channel recording the temperature
+    float temperatureInF = ThingSpeak.readFloatField(weatherStationChannelNumber, temperatureFieldNumber);
 
-  // Read in field 1 of the private channel which is a counter  
-  long count = ThingSpeak.readLongField(counterChannelNumber, counterFieldNumber, myCounterReadAPIKey);  
+    // Check the status of the read operation to see if it was successful
+    statusCode = ThingSpeak.getLastReadStatus();
+    if (statusCode == 200)
+    {
+        Serial.println("Temperature at MathWorks HQ: " + String(temperatureInF) + " deg F");
+    }
+    else
+    {
+        Serial.println("Problem reading channel. HTTP error code " + String(statusCode));
+    }
 
-   // Check the status of the read operation to see if it was successful
-  statusCode = ThingSpeak.getLastReadStatus();
-  if(statusCode == 200){
-    Serial.println("Counter: " + String(count));
-  }
-  else{
-    Serial.println("Problem reading channel. HTTP error code " + String(statusCode)); 
-  }
-  
-  delay(15000); // No need to read the counter too often.
-  
+    delay(15000); // No need to read the temperature too often.
+
+    // Read in field 1 of the private channel which is a counter
+    long count = ThingSpeak.readLongField(counterChannelNumber, counterFieldNumber, myCounterReadAPIKey);
+
+    // Check the status of the read operation to see if it was successful
+    statusCode = ThingSpeak.getLastReadStatus();
+    if (statusCode == 200)
+    {
+        Serial.println("Counter: " + String(count));
+    }
+    else
+    {
+        Serial.println("Problem reading channel. HTTP error code " + String(statusCode));
+    }
+
+    delay(15000); // No need to read the counter too often.
 }
