934963bacea1fbfdc057cef0fad6088058abd492
stephen5ng <110236404+stephen5ng@users.noreply.github.com>
2022-09-08 13:58:30 -0700

    Remove ethernet things.

--- ThingSpeak.ino
+++ ThingSpeak.ino
@@ -23,17 +23,10 @@
   Copyright 2020, The MathWorks, Inc.
 */
 
-#include <Ethernet.h>
-#include "secrets.h"
+ #include "secrets.h"
 #include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
 
 byte mac[] = SECRET_MAC;
-
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
-IPAddress myDns(192, 168, 0, 1);
-
-EthernetClient client;
 
 // Weather station channel details
 unsigned long weatherStationChannelNumber = SECRET_CH_ID_WEATHER_STATION;
@@ -45,36 +38,12 @@ const char * myCounterReadAPIKey = SECRET_READ_APIKEY_COUNTER;
 unsigned int counterFieldNumber = 1; 
 
 void setup() {
-  Ethernet.init(10);  // Most Arduino Ethernet hardware
   Serial.begin(115200);  //Initialize serial
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo native USB port only
   }
       
-  // start the Ethernet connection:
-  Serial.println("Initialize Ethernet with DHCP:");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      while (true) {
-        delay(1); // do nothing, no point running without Ethernet hardware
-      }
-    }
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // try to congifure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip, myDns);
-  } else {
-    Serial.print("  DHCP assigned IP ");
-    Serial.println(Ethernet.localIP());
-  }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  
-  ThingSpeak.begin(client);  // Initialize ThingSpeak
+//   ThingSpeak.begin(client);  // Initialize ThingSpeak
 }
 
 void loop() {
